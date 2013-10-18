cde298a511abe29194f22492a1bc3e1d0a6bd219
Etara <evo960225@gmail.com>
2013-10-19 01:12:50 +0800

    AC (3ms, 188KB)

---
+++ 基礎題庫/a017.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a;
+	while(cin>>a){
+		if(a>=90){
+			cout<<"A"<<endl;
+		}else if(a>=80){
+			cout<<"B"<<endl;
+		}else if(a>=70){
+			cout<<"C"<<endl;
+		}else if(a>=60){
+			cout<<"D"<<endl;
+		}else{
+			cout<<"E"<<endl;
+		}
+	}
+	return 0;
+}
+
