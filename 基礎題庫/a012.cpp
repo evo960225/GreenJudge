debcd293ce5e3ecae0a14c241cd54da5bfcec506
Etara <evo960225@gmail.com>
2013-10-19 01:12:19 +0800

    AC (3ms, 188KB)

---
+++ 基礎題庫/a012.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	while(cin>>n){
+		if(n%2==1){
+			cout<<"ODD"<<endl; 
+		}else{
+			cout<<"EVEN"<<endl;
+		}
+	}
+	return 0;
+}
+
