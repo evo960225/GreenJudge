8e336038327c2ad01f780a1643f1a3991c20e9a5
Etara <evo960225@gmail.com>
2013-10-22 17:55:37 +0800

    AC (4ms, 188KB)

---
+++ 初級題庫/b013.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	string st1,st2,st3;
+	getline(cin,st1);
+	getline(cin,st2);
+	getline(cin,st3);
+	cout<<st3<<endl;
+	cout<<st1<<endl;
+	cout<<st2<<endl;
+	
+	return 0;
+}
+
